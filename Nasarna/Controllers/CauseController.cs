4b089654b1ed7af5ca18b6bd4d990b9ecacb752b
yusifizcode <5ba0gh2@code.edu.az>
2022-08-13 18:51:58 +0400

    project added

--- Nasarna/Controllers/CauseController.cs
+++ Nasarna/Controllers/CauseController.cs
@@ -117,6 +117,111 @@ namespace Nasarna.Controllers
             return RedirectToAction("index");
         }
 
+
+        public IActionResult Edit(int id)
+        {
+            var cause = _context.Causes.Include(x => x.CauseTags).ThenInclude(t => t.Tag).Include(x => x.Category).Include(x => x.CauseImages).FirstOrDefault(x => x.Id == id);
+
+            if (cause == null)
+                return RedirectToAction("error", "dashboard");
+
+            ViewBag.Categories = _context.Categories.ToList();
+            ViewBag.Tags = _context.Tags.ToList();
+
+            cause.TagIds = cause.CauseTags.Select(x => x.TagId).ToList();
+            return View(cause);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Cause cause)
+        {
+            var existCause = _context.Causes.Include(x => x.CauseTags).ThenInclude(t => t.Tag).Include(x => x.Category).Include(x => x.CauseImages).FirstOrDefault(x=>x.Id == cause.Id);
+
+            if (existCause == null)
+                return RedirectToAction("error", "dashboard");
+
+
+            if (cause.ImageFiles != null)
+            {
+                foreach (var file in cause.ImageFiles)
+                {
+                    if (file.ContentType != "image/png" && file.ContentType != "image/jpeg")
+                    {
+                        ModelState.AddModelError("ImageFiles", "File format must be image/png or image/jpeg");
+                    }
+
+                    if (file.Length > 2097152)
+                    {
+                        ModelState.AddModelError("ImageFiles", "File size must be less than 2MB");
+                    }
+                }
+            }
+
+            if (cause.TagIds != null)
+            {
+                foreach (var tagId in cause.TagIds)
+                {
+                    if (!_context.Tags.Any(x => x.Id == tagId))
+                    {
+                        ModelState.AddModelError("TagIds", "Tag id not found");
+                    }
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = _context.Categories.ToList();
+                ViewBag.Tags = _context.Tags.ToList();
+                return View();
+            }
+
+            existCause.CauseTags.RemoveAll(bt => !existCause.TagIds.Contains(bt.Id));
+
+            foreach (var tagId in cause.TagIds.Where(x => !existCause.CauseTags.Any(bt => bt.TagId == x)))
+            {
+                CauseTag causeTag = new CauseTag
+                {
+                    TagId = tagId
+                };
+                existCause.CauseTags.Add(causeTag);
+            }
+
+            if (cause.ImageFiles != null)
+            {
+                if (existCause.CauseImages != null)
+                {
+                    foreach (var file in existCause.CauseImages)
+                    {
+                        FileManager.Delete(_env.WebRootPath, "uploads/causes", file.Name);
+                        _context.CauseImages.Remove(file);
+                    }
+                }
+
+                foreach (var file in cause.ImageFiles)
+                {
+                    CauseImage causeImage = new CauseImage
+                    {
+                        Name = FileManager.Save(_env.WebRootPath, "uploads/causes", file),
+                    };
+
+                    existCause.CauseImages.Add(causeImage);
+                }
+            }
+
+
+            existCause.Address = cause.Address;
+            existCause.Desc = cause.Desc;
+            existCause.SubDesc = cause.SubDesc;
+            existCause.Title = cause.Title;
+            existCause.SubTitle = cause.SubTitle;
+            existCause.CategoryId = cause.CategoryId;
+
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+
         [HttpPost]
         public IActionResult Donate(CauseDetailViewModel detailVM)
         {
@@ -145,9 +250,5 @@ namespace Nasarna.Controllers
             return RedirectToAction("index","home");
         }
 
-        public IActionResult Edit()
-        {
-            return View();
-        }
     }
 }
